ac89f268f8714a882fe9fce6dfbf9c8eac30222b
Wassim Mansouri <wassim.mansouri@gmail.com>
2020-09-19 18:33:44 +0200

    Add the main image content.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         backgroundColor: Colors.blueGrey[900],
         title: Text('I Am Rich'),
         centerTitle: true,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
       ),
     ),
   ));
